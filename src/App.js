ef71d574593e7b791b5542933b23b15c0ac22f74
Alexis1226 <feuri41@gmail.com>
2023-08-22 01:06:04 +0900

    Set up & Basic Use with Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const { data, error, isLoading } = useQuery(
+    ["text"],
+    () => {
+      return axios(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=krw"
+      );
+    }
+  );
+  return (
+    <div className="App">
+      {error ? <p>Oops! Error bumped!</p> : null}
+      {isLoading ? <p>Wait! Still Loading...</p> : null}
+      {data?.data?.bitcoin?.krw
+        ? data.data.bitcoin.krw
+        : null}
+    </div>
+  );
 }
 
 export default App;
